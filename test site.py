dac84ad50277691e79a1d213fdc653498a8e5a7f
vetkan <bandarupallivenkat@yahoo.com>
2019-08-27 21:19:23 -0500

    addition of templates

--- test site.py
+++ test site.py
@@ -1,17 +1,25 @@
 
 #Flask is class and flask is framework
 
-from flask import Flask
+from flask import Flask, render_template
 
 #variable to store Flask class instance
+#render_template access HTML file, stores in the python App file and displays it on the URL
 
 app=Flask(__name__)
 
 # / is home page
 @app.route('/')
-
 def home():
-	return "Website content goes here"
+	return render_template("home.html")
+
+#functions cannot have same name i.e. home, about funcs
+
+@app.route('/aboutme/')
+def about():
+	return render_template("about.html")
 
 if __name__=="__main__":
 	app.run(debug=True)
+
+# you don't have to close whenever you make changes to the web app script.If the script is running it automatically refreshes.
